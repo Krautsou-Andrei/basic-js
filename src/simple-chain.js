af64adacecac95af5714bdf91970a202f2cebdd8
Andrei Krautsou <109202914+Krautsou-Andrei@users.noreply.github.com>
2023-04-11 21:00:10 +0300

    feat: add simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,31 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 const chainMaker = {
+  array: [],
   getLength() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    return this.array.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+
+  addLink(value) {
+    this.array.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (isNaN(position) || position < 1 || position > this.array.length || !Number.isInteger(position)) {
+      this.array = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    this.array.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    this.array.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    const newArray = [...this.array];
+    this.array = [];
+    return newArray.join("~~");
   },
 };
 
